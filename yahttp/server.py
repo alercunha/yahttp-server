22fc132ee43513223f1bf87caa7410597708c83a
Alexandre Cunha <alexandre.cunha@gmail.com>
2016-07-11 23:56:36 -0500

    - cors is now optional (false by default)

--- yahttp/server.py
+++ yahttp/server.py
@@ -3,9 +3,11 @@ import argparse
 from tornado import ioloop, web
 
 
-class CorsEnabledHandler(web.StaticFileHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class MainHandler(web.StaticFileHandler):
+
+    def initialize(self, path, default_filename=None, cors=False):
+        super().initialize(path, default_filename)
+        self.cors = cors
 
     def set_access_preflight_headers(self):
         # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#Preflighted_requests
@@ -20,10 +22,11 @@ class CorsEnabledHandler(web.StaticFileHandler):
 
     def set_extra_headers(self, path):
         headers = self.request.headers
-        if 'Access-Control-Request-Headers' in headers or 'Access-Control-Request-Method' in headers:
-            self.set_access_preflight_headers()
-        if 'Origin' in headers:
-            self.set_access_control_headers()
+        if self.cors:
+            if 'Access-Control-Request-Headers' in headers or 'Access-Control-Request-Method' in headers:
+                self.set_access_preflight_headers()
+            if 'Origin' in headers:
+                self.set_access_control_headers()
 
     def options(self):
         self.set_status(204)
@@ -39,7 +42,7 @@ def run(args):
     }
     # initialize
     handlers = [
-        (r'/(.*)', CorsEnabledHandler, {'path': args.folder, 'default_filename': 'index.html'})
+        (r'/(.*)', MainHandler, {'path': args.folder, 'default_filename': 'index.html', 'cors': args.cors})
     ]
     app = web.Application(handlers, **tornado_settings)
     app.listen(args.port)
@@ -55,6 +58,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-folder', help='directory to server', default='./')
     parser.add_argument('-port', help='http port to listen', default=8080)
+    parser.add_argument('-cors', action='store_true', default=False)
     args = parser.parse_args()
     run(args)
 
